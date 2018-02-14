5526de4bab4816715ccfe1a361775d5ca845aa25
AT <tikonoff@gmail.com>
2018-02-14 23:17:40 +1300

    Approval.sol with mock for receiveApproval() is added

---
+++ contracts/mocks/Approval.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.18;
+
+contract Approval {
+    function receiveApproval(address _address1, uint256 _value, address _address2, bytes _data) public {
+        revert();
+    }
+}
--- test/lab/token.js
+++ test/lab/token.js
@@ -1,13 +1,16 @@
 const HumanStandardToken = artifacts.require("./HumanStandardToken.sol");
+const Approval = artifacts.require("Approval.sol");
+const assertRevert  = require('../helpers/assertRevert');
 
 contract('Token <Blockchain Labs>, @tikonoff', ([owner, user1, user2]) => {
-    let token, value, user, amount, data;
+    let token, value, user, amount, data, approval;
 
     before(async function () {
         user = user1;
         amount = 10;
         data = "";
         token = await HumanStandardToken.new(100000, "LEV", 0, "LEV");
+        approval = await Approval.new();
     });
 
     describe('HumanStandardToken', function () {
@@ -21,10 +24,14 @@ contract('Token <Blockchain Labs>, @tikonoff', ([owner, user1, user2]) => {
             assert.equal(value, amount);
         });
 
-        // it.only('should return false if require fail', async function () {
-        //     assert.isOk(value = await token.approveAndCall.call(0x45C35a63e00e99bde9c48A534F1fFc7860077A76, -1000, "asdfa"));
-        //     assert.equal(value, false);
-        // });
+        it('receiveApproval should revert', async function () {
+            try {
+                await token.approveAndCall(approval.address, 1, "asdf");
+                assert.fail('should have thrown before');
+            } catch(error) {
+                assertRevert(error);
+            }
+        });
     });
 });
 
